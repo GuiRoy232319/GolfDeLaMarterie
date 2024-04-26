4c1049bdb16666bf28079afb4ca9f51f7c380992
GuiRoy232319 <64890072+GuiRoy232319@users.noreply.github.com>
2024-04-26 13:11:41 +0200

    news

---
+++ GolfDeLaMarterie/ViewModel/SignatureView.swift
@@ -0,0 +1,18 @@
+//
+//  SignatureView.swift
+//  GolfDeLaMarterie
+//
+//  Created by guill roy on 26/04/2024.
+//
+
+import SwiftUI
+
+struct SignatureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignatureView()
+}
